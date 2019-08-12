84a86720a53332a57a21986e00a63b45ca478255
Abhik Bhattacharjee <abhikbhattacharjee.mit@gmail.com>
2019-08-12 11:00:40 +0530

    Updated fimo generation and sorted excel file

--- fimo_seq.sh
+++ fimo_seq.sh
@@ -1,11 +1,66 @@
 a=1
-for i in ~/Desktop/BE_Project/out/*.txt
+export PATH=$HOME/meme/bin:$PATH
+
+mkdir output_fimo
+mkdir output_fimo/up_regulated_fimo
+mkdir output_fimo/down_regulated_fimo
+mkdir output_fimo/notdiff_regulated_fimo
+
+for i in ~/Desktop/BE_Project/up_top_tags_out/*.txt
 do	
 	
 	filename=$(basename "$i")
 	fname="${filename%.*}"
-	fimo -o output_fimo/"fimo$fname" jaspar.txt $i
+	fimo -o output_fimo/up_regulated_fimo/"fimo$fname" JASPAR2018_insects_redundant_removed.txt $i
 	#a=$((a+1))
 	#echo $a
 done
+
+for i in ~/Desktop/BE_Project/down_top_tags_out/*.txt
+do	
 	
+	filename=$(basename "$i")
+	fname="${filename%.*}"
+	fimo -o output_fimo/down_regulated_fimo/"fimo$fname" JASPAR2018_insects_redundant_removed.txt $i
+	#a=$((a+1))
+	#echo $a
+done
+
+for i in ~/Desktop/BE_Project/notdiff_top_tags_out/*.txt
+do	
+	
+	filename=$(basename "$i")
+	fname="${filename%.*}"
+	fimo -o output_fimo/notdiff_regulated_fimo/"fimo$fname" JASPAR2018_insects_redundant_removed.txt $i
+	#a=$((a+1))
+	#echo $a
+done
+
+
+for i in ~/Desktop/BE_Project/output_fimo/up_regulated_fimo/*
+do	
+	python ~/Desktop/BE_Project/temp.py $i/*.tsv $i
+	#echo $i
+	#filename=$(basename "$i")
+	#fname="${filename%.*}"
+	#echo $filename
+done
+
+for i in ~/Desktop/BE_Project/output_fimo/down_regulated_fimo/*
+do	
+	python ~/Desktop/BE_Project/temp.py $i/*.tsv $i
+	#echo $i
+	#filename=$(basename "$i")
+	#fname="${filename%.*}"
+	#echo $filename
+done
+
+for i in ~/Desktop/BE_Project/output_fimo/notdiff_regulated_fimo/*
+do	
+	python ~/Desktop/BE_Project/temp.py $i/*.tsv $i
+	#echo $i
+	#filename=$(basename "$i")
+	#fname="${filename%.*}"
+	#echo $filename
+done
+		
