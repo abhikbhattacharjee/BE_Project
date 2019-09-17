d2867c2c6a71f406bf0c9f32212625b71824d33c
Abhik Bhattacharjee <abhikbhattacharjee.mit@gmail.com>
2019-09-17 12:16:55 +0530

    Updated count analysis
    changed position of notdiff, up, and down regulation

--- Automated Module/Overall_count_analysis.py
+++ Automated Module/Overall_count_analysis.py
@@ -12,17 +12,17 @@ df = pd.read_excel (r'Excel/Up_regulation.xlsx')
 """df.drop(df.tail(1).index,inplace=True)"""
 print (df)
 
-diff_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
+up_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
 df = pd.read_excel (r'Excel/Down_regulation.xlsx')
 """df.drop(df.tail(1).index,inplace=True)"""
 print (df)
-up_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
+down_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
 
 df = pd.read_excel (r'Excel/Notdiff_regulation.xlsx')
 """df.drop(df.tail(1).index,inplace=True)"""
 print (df)
 
-down_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
+diff_sums = df.select_dtypes(pd.np.number).sum().rename('total').to_frame()
 
 overall = pd.concat([diff_sums,up_sums,down_sums],axis=1)
 overall.columns = ['not_diff','up','down']
