0845ee5d59aeec9282b9401964ac912294177ef1
Yash Gaglani <43441007+Yash-Gaglani@users.noreply.github.com>
2020-01-20 15:39:35 +0530

    Update heatmap_test.py

--- heatmap_test.py
+++ heatmap_test.py
@@ -1,23 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 15 11:07:09 2020
-
-@author: Abhik
-"""
 
 import pylab as plt
 import numpy as np
 
-fn = "heatmap.txt"
+
+
+fn = "C:\\Project\heatmap1.txt"
 with open(fn, "r") as f:
     labels = f.readline().rstrip("\n").split()[1:]
 data = np.loadtxt(fn, skiprows=1, converters={0:lambda x: 0})
-plot_data = np.ma.masked_equal(data[:,1:], 0)
-
+data[data > 1] = 1
+data[data == 0] =1.100
+plot_data = np.ma.masked_equal(data[:,1:], 0)   
 plt.subplots_adjust(left=0.1, bottom=0.15, right=0.99, top=0.95)
-plt.imshow(plot_data, cmap=plt.cm.get_cmap("Reds"), interpolation="nearest", aspect = "auto")
+plt.imshow(plot_data,cmap= "inferno", interpolation="nearest", aspect = "auto")
+#mpl.rcParams['lines.color'] = 'r'
 plt.xticks(range(len(labels)), labels, rotation=90, va="top", ha="center")
 plt.colorbar()
 
-plt.rcParams["figure.figsize"] = (20,10)
+plt.rcParams["figure.figsize"] = (80,70)
 plt.show()
